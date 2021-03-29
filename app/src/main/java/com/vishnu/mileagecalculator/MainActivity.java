39d30e445708154c541c272964e84a69d9a47841
Vishnu Anand <vishnuanand2000@gmail.com>
2021-03-29 20:44:09 +0530

    Implemented calculation functions and reset functions. App is functional.

--- app/src/main/java/com/vishnu/mileagecalculator/MainActivity.java
+++ app/src/main/java/com/vishnu/mileagecalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.vishnu.mileagecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     //Initializations
     TextView DisplayKPL;
     EditText KMsBeforeFuelFill, KMsRunTillFuelEmpties, AmountOfFuelFilled;
-    Button CalculateMileage;
+    Button CalculateMileage, ResetForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +29,52 @@ public class MainActivity extends AppCompatActivity {
         AmountOfFuelFilled = findViewById(R.id.FuelInput);
 
         //Button to enable calculation
+        //This button should call a method from a class which would output the required result
         CalculateMileage = findViewById(R.id.CalculateBtn);
+        ResetForm = findViewById(R.id.ResetBtn);
+
+
+        //OnClickListener for button
+        CalculateMileage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String BeforeFill = KMsBeforeFuelFill.getText().toString();
+                String RunAfterFill = KMsRunTillFuelEmpties.getText().toString();
+                String FuelFilled = AmountOfFuelFilled.getText().toString();
+
+                //OnClick Invoke a class
+                Calculate(BeforeFill, RunAfterFill, FuelFilled);
+
+            }
+        });
+
+        ResetForm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Reset();
+            }
+        });
 
 
     }
+
+    public void Calculate(String BeforeFill,String RunAfterFill,String FuelFilled){
+
+       int  beforeFill = Integer.parseInt(BeforeFill);
+       int runAfterFill = Integer.parseInt(RunAfterFill);
+       int fuelFilled = Integer.parseInt(FuelFilled);
+
+       int mileage = (runAfterFill-beforeFill)/fuelFilled;
+
+       DisplayKPL.setText(String.valueOf(mileage));
+    }
+
+    public void Reset(){
+        DisplayKPL.setText("0");
+        KMsBeforeFuelFill.getText().clear();
+        KMsRunTillFuelEmpties.getText().clear();
+        AmountOfFuelFilled.getText().clear();
+    }
+
 }
